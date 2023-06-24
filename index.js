2907014241b800869d96680c9a7eebead26af076
ShreeSamal <shree.samal1502@gmail.com>
2023-06-25 00:59:19 +0530

    initial

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('Hello from Node.js!');
 });
 
